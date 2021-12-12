76d08c41aa5d78598b8c7248c5c699eee6b6a60a
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-12-12 10:34:17 +0100

    BookDbManager created and Connect method implemented

---
+++ src/Codecool.BookDb/Manager/BookDbManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+using Microsoft.Data.SqlClient;
+
+namespace Codecool.BookDb.Manager
+{
+    public class BookDbManager
+    {
+        public string ConnectionString => ConfigurationManager.AppSettings["connectionString"];
+
+        public string Connect()
+        {
+            using var connection = new SqlConnection(ConnectionString);
+            connection.Open();
+            Console.WriteLine("Connected...");
+            return ConnectionString;
+        }
+    }
+}
